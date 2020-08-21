649f17f0f9584a6fa912d1ba9233674e346ded4e
dinijain <46283488+dinijain@users.noreply.github.com>
2020-08-21 23:09:05 +0530

    Update fibonacci_sum_last_digit.cpp

--- fibonacci_sum_last_digit.cpp
+++ fibonacci_sum_last_digit.cpp
@@ -1,25 +1,46 @@
+
 #include <iostream>
 
-int fibonacci_sum_naive(long long n) {
+/*int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
         return n;
 
-    long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
+    int previous = 0;
+    int current  = 1;
 
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous;
         previous = current;
-        current = tmp_previous + current;
-        sum += current;
+        current = (tmp_previous + current)%10;
     }
 
-    return sum % 10;
+    return current ;
+}*/
+int get_fibonacci_last_digit_naive(long long n) {
+    n=n % 60;         //pattern for last digit of fibonacci series repeats after every 60 fibonacci numbers i.e. f(61)%10=f(1)%10
+    if (n <= 1)
+        return n;
+
+    int a = 0,c;
+    int b  = 1;
+    int s=1;
+
+    for (int i = 0; i < n-1; ++i) {
+               c=(a+b)%10;
+                s+=c;
+                 s=s%10;
+        a=b;
+        b=c;
+
+    }
+
+    return s ;
 }
 
 int main() {
-    long long n = 0;
+    long long n,s=0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
-}
+
+        std::cout <<  get_fibonacci_last_digit_naive(n) << '\n';
+
+    }
